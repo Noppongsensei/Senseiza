df8ccc8e189ad65b0c2bae6c70bdc3632fb304d4
Sensei <noppongsen@gmail.com>
2023-12-12 16:17:23 +0700

    Lab 8.3

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,3 +1,46 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int Age;
+    int Tall;
+    int V;
+    cout<<"Enter your age: ";
+    cin>>Age;
+    if(Age<=25&&Age<60){
+        cout<<"Enter your height: ";
+        cin >> Tall;
+        if(Tall<100)
+            cout << "Your character = Chopper";
+        else if(Tall<180&&Tall>=100){
+            cout << "Your character = Usopp";
+        }else{
+            cout<<"Enter your bounty: " ;  
+            cin>>V;
+            if(V>1100000000)
+            cout << "Your character = Zoro";
+                else
+                cout << "Your character = Sanji";
+            }
+    }else{
+            if(Age>25&&Age<=60){
+                cout<<"Enter your bounty: " ;
+                cin>>V;
+                if(V>500000000){
+                    cout<<"Your character = Jinbe";
+                    }else
+                        cout << "Your character = Franky";
+                    
+            }else
+                cout << "Your character = Brook";     
+        return 0;
+    }
+}
+
+
+
+
+
+
 /*
 "Enter your age: "
 "Enter your height: "
